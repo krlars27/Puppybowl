e6655ff7c22e9677a01c27f0f702c0df5d4c29c6
Robby Bacus <robbybacus@gmail.com>
2022-10-13 14:00:44 -0400

    3rd commit

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,30 @@
-import React from "react";
-import {Navbar, Puppylist} from './'
+import React, { useEffect, useState } from "react";
+import { Navbar, Puppylist } from './';
+
+
+
 
 
 const Main = () => {
+
+  const [puppyData, setPuppyData] = useState([]);
+  useEffect(() => {
+    async function getPuppyData() {
+      const response = await fetch(
+        "https://fsa-puppy-bowl.herokuapp.com/api/2209-FTB-ET-WEB-FT/players"
+      );
+      const result = await response.json();
+      const puppies =  result.data.players
+      setPuppyData(puppies)
+
+    }
+    getPuppyData();
+  }, []);
+  
   return (
     <div id="main">
     <Navbar/>
-    <Puppylist/>
+    <Puppylist puppyData ={puppyData}/>
   </div>
   );
 };
--- src/components/Puppylist.jsx
+++ src/components/Puppylist.jsx
@@ -1,39 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
-const fetchPuppyData = (props) => {
-  const [data, setData] = useState();
-  useEffect(() => {
-    async function getData() {
-      let response = await fetch(
-        "https://fsa-puppy-bowl.herokuapp.com/api/2209-FTB-ET-WEB-FT/players"
-      );
-      let puppyData = await response.json();
-      setData(puppyData);
-    }
-    getData();
-  }, []);
-  // return (
-  //   <div className="puppy" >{data ? data.body : 'Loading data'}</div>
-  // )
-};
-
-const Puppylist = () => {
-  return (
-    <div>
-      <h2>I am Puppylist</h2>
-      <div id="puppies">
-        <div className="puppy">
-          {data ? data.body : "Loading data"}
-          {fetchPuppyData}
-        </div>
-        {/* <p>Puppy Name</p>
-        <p>Puppy Id</p> */}
-        {/* <img src="puppy1.jpeg" alt="" /> */}
-        {/* <button>See Details</button>
-      <button>Delete from Roster</button> */}
-      </div>
-    </div>
-  );
+const Puppylist = (props) => {
+  const puppyData = props.puppyData
+  console.log (puppyData)
+  return <div>I am puppylist component</div>;
 };
 
 export default Puppylist;
